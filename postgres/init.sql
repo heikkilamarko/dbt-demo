b3860e3b3b27b834f29b4b621c00a53dd1b6d587
Marko Heikkilä <heikkila_marko@hotmail.com>
2025-04-05 15:07:56 +0300

    Cleanup

--- postgres/init.sql
+++ postgres/init.sql
@@ -1,13 +1,1 @@
 CREATE DATABASE demo_db;
-
-\c demo_db;
-
-CREATE SCHEMA demo;
-
-CREATE TABLE demo.raw_employees (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    full_name TEXT NOT NULL,
-    age INT NOT NULL,
-    description TEXT NOT NULL,
-    created_at TIMESTAMPTZ NOT NULL
-);
